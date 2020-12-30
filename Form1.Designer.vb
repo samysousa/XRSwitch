74a2c9dadcde281c9900cb768fce4b58026dcc96
Samuel Sousa <samuel@sousa.net>
2020-12-30 00:51:39 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSetRuntime = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.lblCurrentSetting = New System.Windows.Forms.Label()
+        Me.lbl64bitWaring = New System.Windows.Forms.Label()
+        Me.lblfixed1 = New System.Windows.Forms.Label()
+        Me.lblRunTimeName = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl_help = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSetRuntime
+        '
+        Me.btnSetRuntime.Location = New System.Drawing.Point(423, 21)
+        Me.btnSetRuntime.Name = "btnSetRuntime"
+        Me.btnSetRuntime.Size = New System.Drawing.Size(154, 35)
+        Me.btnSetRuntime.TabIndex = 0
+        Me.btnSetRuntime.Text = "Set OpenXR Runtime"
+        Me.btnSetRuntime.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(22, 28)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(379, 24)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'lblCurrentSetting
+        '
+        Me.lblCurrentSetting.AutoSize = True
+        Me.lblCurrentSetting.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCurrentSetting.Location = New System.Drawing.Point(23, 124)
+        Me.lblCurrentSetting.Name = "lblCurrentSetting"
+        Me.lblCurrentSetting.Size = New System.Drawing.Size(42, 15)
+        Me.lblCurrentSetting.TabIndex = 2
+        Me.lblCurrentSetting.Text = "Label1"
+        '
+        'lbl64bitWaring
+        '
+        Me.lbl64bitWaring.AutoSize = True
+        Me.lbl64bitWaring.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl64bitWaring.ForeColor = System.Drawing.Color.Red
+        Me.lbl64bitWaring.Location = New System.Drawing.Point(22, 9)
+        Me.lbl64bitWaring.Name = "lbl64bitWaring"
+        Me.lbl64bitWaring.Size = New System.Drawing.Size(306, 13)
+        Me.lbl64bitWaring.TabIndex = 3
+        Me.lbl64bitWaring.Text = "Running in 32Bit Mode - Settings could be unreliable"
+        Me.lbl64bitWaring.Visible = False
+        '
+        'lblfixed1
+        '
+        Me.lblfixed1.AutoSize = True
+        Me.lblfixed1.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblfixed1.Location = New System.Drawing.Point(22, 97)
+        Me.lblfixed1.Name = "lblfixed1"
+        Me.lblfixed1.Size = New System.Drawing.Size(279, 18)
+        Me.lblfixed1.TabIndex = 4
+        Me.lblfixed1.Text = "Currently Detected Runtime Location"
+        '
+        'lblRunTimeName
+        '
+        Me.lblRunTimeName.AutoSize = True
+        Me.lblRunTimeName.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRunTimeName.ForeColor = System.Drawing.Color.DarkGreen
+        Me.lblRunTimeName.Location = New System.Drawing.Point(347, 148)
+        Me.lblRunTimeName.Name = "lblRunTimeName"
+        Me.lblRunTimeName.Size = New System.Drawing.Size(85, 18)
+        Me.lblRunTimeName.TabIndex = 5
+        Me.lblRunTimeName.Text = "Detected :"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.AlwaysUnderline
+        Me.LinkLabel1.LinkVisited = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(23, 149)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(145, 16)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "________MY LINK _______"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(380, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(201, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = " Run As Admin to change Runtime"
+        '
+        'lbl_help
+        '
+        Me.lbl_help.AutoSize = True
+        Me.lbl_help.Font = New System.Drawing.Font("Century Gothic", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_help.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.lbl_help.Location = New System.Drawing.Point(399, 2)
+        Me.lbl_help.Name = "lbl_help"
+        Me.lbl_help.Size = New System.Drawing.Size(19, 21)
+        Me.lbl_help.TabIndex = 8
+        Me.lbl_help.Text = "?"
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(585, 181)
+        Me.Controls.Add(Me.lbl_help)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.lbl64bitWaring)
+        Me.Controls.Add(Me.lblCurrentSetting)
+        Me.Controls.Add(Me.lblfixed1)
+        Me.Controls.Add(Me.lblRunTimeName)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.btnSetRuntime)
+        Me.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.Padding = New System.Windows.Forms.Padding(5)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "OPenXR Runtime Switcher - Sam"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSetRuntime As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents lblCurrentSetting As Label
+    Friend WithEvents lbl64bitWaring As Label
+    Friend WithEvents lblfixed1 As Label
+    Friend WithEvents lblRunTimeName As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbl_help As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,170 @@
+﻿Public Class Form1
+
+    Public CurentRuntimeKeyPath As String = "HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\OpenXR\1\"
+    Public CurrentRuntimeKeyValuename As String = "ActiveRuntime"
+    Public ErrorMsgHandle As String = "No Permission to Read - Try Running as Admin"
+    Public myEnv = Environment.Is64BitProcess   '' This is TRUE if you are running in 64Bit
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '' Avoid 32 Bit trouble.
+#Disable Warning IDE0059 ' Unnecessary assignment of a value
+        If Not myEnv Then
+            lbl64bitWaring.Visible = True
+            lbl64bitWaring.Refresh()
+            MsgBox("This program is running in 32 Bit Mode - I won`t touch it unless you are in 64Bit Mode")
+        End If
+
+        '' Assign Links to Link Labels
+        LinkLabel1.Text = "Validate with OpenXR Dev Tools from Microsoft Store"
+        LinkLabel1.Links.Add(14, 37, "ms-windows-store://pdp/?ProductId=9n5cvvl23qbt")
+
+        '' Populate ComboBoxes
+        ComboBox1.Items.Clear()
+        ComboBox1.Items.Add("1 - Steam VR")
+        ComboBox1.Items.Add("2 - Oculus (Default)")
+        ComboBox1.Items.Add("3 - Oculus in D:\Oculus")
+        ComboBox1.Items.Add("4 - Windows Mixed Reality")
+        ComboBox1.Text = "Select from..."
+
+
+        '' Initiate the Reader and 1st Read 
+        Init()
+
+
+
+#Enable Warning IDE0059 ' Unnecessary assignment of a value
+    End Sub
+
+    Private Function DetermineEngineArray(XrRuntimeString As String)
+
+#Disable Warning IDE0059 ' Unnecessary assignment of a value
+        '' I tried to to Select Cases here, but it just did not work out for .
+        '' Good old IF's .. Sorry
+
+        Dim EngineName As String = ""
+        Dim tstEnginename As Boolean = False
+        Dim EngineIndexPos As Integer = 0
+        Dim tst1 As String = "oculus"
+        Dim tst2 As String = "steam"
+        Dim tst3 As String = "MixedReality"
+        Dim tst4 As String = "D:\Oculus\"
+
+
+        If XrRuntimeString.Contains(tst1) Then
+            EngineName = "Oculus Runtime"
+
+            If XrRuntimeString.Contains(tst4) Then
+                EngineIndexPos = 2
+            Else
+                EngineIndexPos = 1
+            End If
+            tstEnginename = True
+        End If
+
+        If XrRuntimeString.Contains(tst2) Then
+            EngineName = "SteamVR Runtime"
+            EngineIndexPos = 0
+            tstEnginename = True
+        End If
+
+        If XrRuntimeString.Contains(tst3) Then
+            EngineName = "Windows Mixed Reality Runtime"
+            EngineIndexPos = 3
+            tstEnginename = True
+        End If
+
+        If tstEnginename = False Then
+            EngineName = "Not Detected"
+            EngineIndexPos = 0
+        End If
+
+
+
+
+        DetermineEngineArray = {EngineIndexPos, EngineName, XrRuntimeString}
+
+#Enable Warning IDE0059 ' Unnecessary assignment of a value
+    End Function
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start(e.Link.LinkData.ToString())
+    End Sub
+
+    Private Sub btnSetRuntime_Click(sender As Object, e As EventArgs) Handles btnSetRuntime.Click
+
+        '' Ok This Should Set the Runtime
+        '' ComboBox Index Perspective
+        '0 - Steam VR
+        '1 - Oculus in C:\Program Files\Oculus
+        '2 - Oculus in D:\Oculus
+        '3 - Windows Mixed Reality
+
+        '        D:\Oculus\Support\oculus-runtime\oculus_openxr_64.json   
+        '        C:\Program Files\Oculus\Support\oculus-runtime\oculus_openxr_64.json
+        '        C:\Program Files (x86)\Steam\steamapps\common\SteamVR\steamxr_win64.json
+        '        C:\WINDOWS\system32\MixedRealityRuntime.json
+
+        Dim InputIndex As Integer
+        Dim PathtoUse As String
+
+        InputIndex = ComboBox1.SelectedIndex
+
+        Select Case InputIndex
+            Case 0
+                PathtoUse = "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\steamxr_win64.json"
+            Case 1
+                PathtoUse = "C:\Program Files\Oculus\Support\oculus-runtime\oculus_openxr_64.json"
+            Case 2
+                PathtoUse = "D:\Oculus\Support\oculus-runtime\oculus_openxr_64.json"
+            Case 3
+                PathtoUse = "C:\WINDOWS\system32\MixedRealityRuntime.json"
+            Case Else
+                PathtoUse = "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\steamxr_win64.json"  '' SteamVR default for Compat
+        End Select
+
+
+        '' Set the New selected Runtime
+        SetRuntime(PathtoUse)
+
+
+
+    End Sub
+
+
+    Private Sub Init()
+
+        '' Detect Initial Runtime at Load
+        Dim myCurRuntimeValue As String
+        Dim detEngeineName = {0, "NameEngine", "Path"}
+        myCurRuntimeValue = Microsoft.Win32.Registry.GetValue(CurentRuntimeKeyPath, CurrentRuntimeKeyValuename, ErrorMsgHandle).ToString
+        lblCurrentSetting.Text = myCurRuntimeValue
+        detEngeineName = DetermineEngineArray(myCurRuntimeValue)
+        '' Set combo Box to Determined Value
+
+        ComboBox1.SelectedIndex = detEngeineName(0)
+        lblRunTimeName.Text = detEngeineName(1)
+
+    End Sub
+
+
+    Private Sub SetRuntime(strPath As String)
+
+        Microsoft.Win32.Registry.SetValue(CurentRuntimeKeyPath, CurrentRuntimeKeyValuename, strPath)
+
+        '' Start Init Again to Reflect The Changes
+
+        Init()
+
+    End Sub
+
+    Private Sub lbl_help_Click(sender As Object, e As EventArgs) Handles lbl_help.Click
+
+        Dim message As String
+        message = "Select the runtime you wish to set for OpenXR" & vbCrLf
+        message = message & "You might Need to Run As Admin to be able to make changes" & vbCrLf
+        message = message & "To Validate use the Microsoft Dev Tool Link Below." & vbCrLf
+        message = message & "Oculus or Steam VR needs to be running to show as Validated" & vbCrLf
+        message = message & "No need to Reboot." & vbCrLf
+        MsgBox(message)
+    End Sub
+End Class
